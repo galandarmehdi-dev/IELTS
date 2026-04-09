437dcdf3290c1ca94fbcc7bced5f4ce5caa14b91
Galandar Mehdiyev <galandar@Galandars-MacBook-Air.local>
2026-04-09 20:09:10 +0400

    Protect remaining tests behind worker

--- assets/js/tests/registry.js
+++ assets/js/tests/registry.js
@@ -24,7 +24,7 @@
     historyUpdateMs: 15000,
   };
 
-  const REMOTE_TEST_CONTENT = new Set(["ielts5", "ielts6", "ielts7"]);
+  const REMOTE_TEST_CONTENT = new Set(["ielts1", "ielts2", "ielts3", "ielts4", "ielts5", "ielts6", "ielts7"]);
   const remoteTestContentPromises = new Map();
 
   const POLLING = {

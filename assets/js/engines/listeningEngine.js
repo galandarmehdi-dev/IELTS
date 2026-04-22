7d5aded3999f51136822f5f43f320b5652b9e29a
galandarmehdi-dev <galandar.mehdi@gmail.com>
2026-04-22 12:06:16 +0400

    Update listeningEngine.js

--- assets/js/engines/listeningEngine.js
+++ assets/js/engines/listeningEngine.js
@@ -914,7 +914,7 @@ function applyActiveListeningContent() {
         R() && (
           isPracticeListening
             ? R().TEMP_STUDENT_AUDIO_SCRUB_PRACTICE === true
-            : R().TEMP_STUDENT_AUDIO_SCRUB_FULL === true
+            : R().TEMP_STUDENT_AUDIO_SCRUB_FULL === false
         )
       );
       const allowControls = isAdmin || allowStudentScrub;
